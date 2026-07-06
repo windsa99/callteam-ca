0792c12ba36866c2efca7293178bd76eee384c93
windsa99 <hello@callteam.ca>
2026-07-05 19:15:54 -0600

    Add files via upload

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = function (eleventyConfig) {
+  // Copy the assets folder as-is (CSS, images) into the final output
+  eleventyConfig.addPassthroughCopy("src/assets");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      data: "_data"
+    },
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
+  };
+};
